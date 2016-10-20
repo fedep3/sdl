7a3e2dba41c8675e2061e1cf1a8bcaf6cba04305
Jenna MacCarley <jenna@Jennas-MacBook-Pro.local>
2016-10-20 14:57:19 -0700

    added some comparison of regression algs code - broken

--- main.py
+++ main.py
@@ -2,14 +2,57 @@
 # -*- coding: utf-8 -*-
 
 from readers.mat_reader import MatReader
+import matplotlib.pyplot as plt
+from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
 
 def main(mat_folder_path):
     mat_reader = MatReader(mat_folder_path)
     xs, ys = mat_reader.read()
     print '# of x = %d, # of y = %d' % (len(xs), len(ys))
+    #print xs[0], xs[1], xs[2]
+    #print ys[0], ys[1], ys[2]
+    cmp_regression_algs(xs, ys)
 
+def cmp_regression_algs(xs, ys):
+# prepare configuration for cross validation test harness
+  num_folds = 10
+  num_instances = len(xs)
+  seed = 7
+# prepare models
+  models = []
+  models.append(('LR', LogisticRegression()))
+  models.append(('LDA', LinearDiscriminantAnalysis()))
+  models.append(('KNN', KNeighborsClassifier()))
+  models.append(('CART', DecisionTreeClassifier()))
+  models.append(('NB', GaussianNB()))
+  models.append(('SVM', SVC()))
+# evaluate each model in turn
+  results = []
+  names = []
+  scoring = 'accuracy'
+  for name, model in models:
+    kfold = cross_validation.KFold(n=num_instances, n_folds=num_folds, random_state=seed)
+    cv_results = cross_validation.cross_val_score(model, xs, ys, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+# boxplot algorithm comparison
+  fig = plt.figure()
+  fig.suptitle('Algorithm Comparison')
+  ax = fig.add_subplot(111)
+  plt.boxplot(results)
+  ax.set_xticklabels(names)
+  plt.show()
 
 # Simple example on how to read the data.
 if __name__ == "__main__":
     main('ColdComplaintData/Training')
+
