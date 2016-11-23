10cb6eecc7735c11b6504d8806d2724e0a662d27
Federico Ponte <fedep3@gmail.com>
2016-11-23 09:01:40 -0800

    Separed a little bit mode main methods and added the best combination

--- main.py
+++ main.py
@@ -23,15 +23,20 @@ def main():
     mat_reader = MatReader()
     training_ts, training_xs, training_ys = mat_reader.read('ColdComplaintData/Training')
 
-    if __debug__:
-        RegressionToolbox.compare_regression_algorithms(training_xs, training_ys, 5)
-
-    # Build the ideal linear regression model
     regression_model = LinearRegression()
     regression_model.fit(training_xs, training_ys)
 
-    future_prediction_model_results = []
+    testing_ts, testing_xs, testing_ys = mat_reader.read('ColdComplaintData/Testing')
+    future_prediction_model = ARIMAFuturePredictionModel(FUTURE_PREDICTION_HORIZON, 1, 0)
+    detect(regression_model, future_prediction_model, 64, testing_ts, testing_xs, testing_ys)
 
+
+def compare_regression_algorithms(training_xs, training_ys):
+    RegressionToolbox.compare_regression_algorithms(training_xs, training_ys, 5)
+
+
+def compare_detection_algorithms(regression_model, ts, xs, ys):
+    future_prediction_model_results = []
     for future_prediction_model in [ARIMAFuturePredictionModel(FUTURE_PREDICTION_HORIZON, 1, 1),
                                     ARIMAFuturePredictionModel(FUTURE_PREDICTION_HORIZON, 1, 0),
                                     ARIMAFuturePredictionModel(FUTURE_PREDICTION_HORIZON, 2, 0),
@@ -43,30 +48,33 @@ def main():
             if isinstance(future_prediction_model, AggregatingFuturePredictionModel) and past_prediction_horizon < 64:
                 continue
             print 'Past prediction horizon: ', past_prediction_horizon
-            detection_toolbox = DetectionToolbox(regression_model, past_prediction_horizon, future_prediction_model)
-
-            # Calculate residuals and predict alarms using validation data
-            # Using validation or training data for prediction?
-            testing_ts, testing_xs, testing_ys = mat_reader.read('ColdComplaintData/Testing')
-            if __debug__:
-                print 'Actuals: ', testing_ys
-            future_residuals_prediction = detection_toolbox.predict_residuals(testing_ts, testing_xs, testing_ys)
-            if __debug__:
-                print 'Future residual predictions: ', future_residuals_prediction
-
-            roc_auc, threshold, fa_rate, md_rate = detection_toolbox.calculate_roc_curve(future_residuals_prediction, testing_ys)
-            print 'ROC AUC: ', roc_auc
-            print 'Idea threshold found: ', threshold
-            print 'FA rate: %0.2f, MD rate: %0.2f' % (fa_rate, md_rate)
-
-            future_prediction_model_results.append((future_prediction_model, past_prediction_horizon, threshold, fa_rate, md_rate))
-            print 'Model=%s, Past Prediction Horizon=%s, Threshold=%s, FA=%s, MD=%s' % future_prediction_model_results[-1]
+            fa_rate, md_rate, threshold = detect(regression_model, future_prediction_model, past_prediction_horizon,
+                                                 ts, xs, ys)
+            future_prediction_model_results.append(
+                (future_prediction_model, past_prediction_horizon, threshold, fa_rate, md_rate))
+            print 'Model=%s, Past Prediction Horizon=%s, Threshold=%s, FA=%s, MD=%s' \
+                  % future_prediction_model_results[-1]
             print '======================'
-
     print 'Sorted results'
-    future_prediction_model_results.sort(key=lambda s: s[3]+s[4])
+    future_prediction_model_results.sort(key=lambda s: s[3] + s[4])
     for future_prediction_model_result in future_prediction_model_results:
         print 'Model=%s, Past Prediction Horizon=%s, Threshold=%s, FA=%s, MD=%s' % future_prediction_model_result
+
+
+def detect(regression_model, future_prediction_model, past_prediction_horizon, ts, xs, ys):
+    detection_toolbox = DetectionToolbox(regression_model, past_prediction_horizon, future_prediction_model)
+    # Calculate residuals and predict alarms using validation data
+    # Using validation or training data for prediction?
+    if __debug__:
+        print 'Actuals: ', ys
+    future_residuals_prediction = detection_toolbox.predict_residuals(ts, xs, ys)
+    if __debug__:
+        print 'Future residual predictions: ', future_residuals_prediction
+    roc_auc, threshold, fa_rate, md_rate = detection_toolbox.calculate_roc_curve(future_residuals_prediction, ys)
+    print 'ROC AUC: ', roc_auc
+    print 'Idea threshold found: ', threshold
+    print 'FA rate: %0.2f, MD rate: %0.2f' % (fa_rate, md_rate)
+    return fa_rate, md_rate, threshold
 
 
 # only perform cross validation between training and validation sets
