9d70185229e08993f377388a1d5d99a7be42facf
Federico Ponte <fedep3@gmail.com>
2016-12-11 21:48:35 -0800

    Fixed issue

--- regression_toolbox.py
+++ regression_toolbox.py
@@ -11,8 +11,16 @@ from pyelm.elm import ELMRegressor
 from readers.mat_reader import MatReader
 
 
-def nothing(x):
-    return x
+class NothingScaler:
+
+    def __init__(self):
+        pass
+
+    def fit(self, x):
+        pass
+
+    def transform(self, x):
+        return x
 
 
 class RegressionToolbox:
@@ -20,23 +28,25 @@ class RegressionToolbox:
     def __init__(self):
         pass
 
-    MODELS = {'SVM': {'constructor': svm.SVR, 'preprocessing': preprocessing.minmax_scale},
-              'ELM': {'constructor': ELMRegressor, 'preprocessing': preprocessing.minmax_scale},
-              'LR': {'constructor': LinearRegression, 'preprocessing': preprocessing.minmax_scale},
-              'KNN': {'constructor': KNeighborsRegressor, 'preprocessing': preprocessing.scale},
-              'BNN': {'constructor': BaggingRegressor, 'preprocessing': preprocessing.scale},
-              'RANSAC': {'constructor': RANSACRegressor, 'preprocessing': nothing}}
+    MODELS = {'SVM': {'constructor': svm.SVR, 'scaler': preprocessing.MinMaxScaler()},
+              'ELM': {'constructor': ELMRegressor, 'scaler': preprocessing.MinMaxScaler()},
+              'LR': {'constructor': LinearRegression, 'scaler': preprocessing.MinMaxScaler()},
+              'KNN': {'constructor': KNeighborsRegressor, 'scaler': preprocessing.StandardScaler()},
+              'BNN': {'constructor': BaggingRegressor, 'scaler': preprocessing.StandardScaler()},
+              'RANSAC': {'constructor': RANSACRegressor, 'scaler': NothingScaler()}}
 
     @staticmethod
     def get_instance(algorithm, training_data_folder, testing_data_folder):
+        if algorithm not in RegressionToolbox.MODELS:
+            raise Exception('Invalid algorithm')
         mat_reader = MatReader()
         training_ts, training_xs, training_ys = mat_reader.read(training_data_folder)
-        algorithm_functions = RegressionToolbox.MODELS[algorithm] if algorithm in RegressionToolbox.MODELS \
-            else RegressionToolbox.MODELS['SVM']
+        algorithm_functions = RegressionToolbox.MODELS[algorithm]
         regression_model = algorithm_functions['constructor']()
-        regression_model.fit(algorithm_functions['preprocessing'](training_xs), training_ys)
+        algorithm_functions['scaler'].fit(training_xs)
+        regression_model.fit(algorithm_functions['scaler'].transform(training_xs), training_ys)
         testing_ts, testing_xs, testing_ys = mat_reader.read(testing_data_folder)
-        return regression_model, testing_ts, algorithm_functions['preprocessing'](testing_xs), testing_ys
+        return regression_model, testing_ts, algorithm_functions['scaler'].transform(testing_xs), testing_ys
 
     @staticmethod
     def compare_regression_algorithms(ps=10):
@@ -46,7 +56,8 @@ class RegressionToolbox:
         scoring = 'neg_mean_squared_error'
         for name, algorithm_functions in RegressionToolbox.MODELS.iteritems():
             model = algorithm_functions['constructor']()
-            cv_results = cross_val_score(model, algorithm_functions['preprocessing'](xs), ys, cv=ps, scoring=scoring)
+            algorithm_functions['scaler'].fit(xs)
+            cv_results = cross_val_score(model, algorithm_functions['scaler'].transform(xs), ys, cv=ps, scoring=scoring)
             results.append(cv_results)
             print '%s: %f (%f)' % (name, cv_results.mean(), cv_results.std())
         fig = plt.figure(figsize=(10, 6))
